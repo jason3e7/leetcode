0a4eed41d7dad6890f949bc84a037777240aa19a
jason3e7 <jason3e7@gmail.com>
2018-02-25 11:22:37 +1300

    70_climbing-stairs AC

--- 70_climbing-stairs.c
+++ 70_climbing-stairs.c
@@ -3,15 +3,33 @@
   * @author Jason3e7
   * @algorithm math
   * @date 201802251100
+  * @note
+  * F(46) 1836311903
+  * F(47) 2971215073
   */
 
 #include <stdio.h>
 
+int num[46] = {0};
+
+int genNum() {
+	int i;
+	num[0] = 1;
+	num[1] = 1;
+	for(i = 2; i < 46; i++) {
+		num[i] = num[i - 1] + num[i - 2];
+	}
+	return 0;
+}
+
 int climbStairs(int n) {
+	if(num[0] == 0) {
+		genNum();
+	}
 	if(n <= 1) {
 		return 1;
 	}
-	return climbStairs(n - 1) + climbStairs(n - 2);    
+	return num[n];    
 }
 
 int main() {
@@ -23,6 +41,6 @@ int main() {
 	printf("%d\n", climbStairs(6));
 	printf("%d\n", climbStairs(7));
 	printf("%d\n", climbStairs(8));
-	printf("%d\n", climbStairs(100));
+	printf("%d\n", climbStairs(45));
 	return 0;
 }
