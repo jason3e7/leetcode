7a06dbc489c70d48402f3338a615669621f98ae5
jason3e7 <jason3e7@gmail.com>
2018-02-27 11:55:05 +1300

    69_sqrtx AC Newton's method

--- 69_sqrtx.c
+++ 69_sqrtx.c
@@ -1,23 +1,37 @@
  /*
   * @file 69_sqrtx.c
   * @author Jason3e7
-  * @algorithm basic
+  * @algorithm Newton's method
   * @date 201802271052
-  * @note sqrt(2147483647) = 46340....
+  * @note
+  * 
+x^2 - a = 0
+(x, f(x)), Derivative 2x
+
+ex : http://www.matrix67.com/blog/archives/361
+(4, 14), 8
+next x => (y2 - y1) / (x2 - x1) = m
+(14 - 0) / (4 - x) = 8
+4 - x = 14 / 8
+x = 4 - 14 / 8 
+x = 2.25
+
+x-f(x)/(2x), f(x)=x^2-a
+x - (x^2-a)/(2x)
+x - x/2 + a/2x
+(x + a/x)/2 
+
+(4 + 2/4)/2 = 2.25
   */
 
 #include <stdio.h>
 
-int mySqrt(int x) {
-	int i, res = 0;
-	for(i = 1; i <= 46340; i++) {
-		if(i * i > x) {
-			break;
-		} else {
-			res = i;
-		}
-	}
-	return res;
+int mySqrt(int x) { 
+    long r = x;
+    while (r * r > x)
+        r = (r + x / r) / 2;
+    return r;
+    
 }
 
 int main() {
