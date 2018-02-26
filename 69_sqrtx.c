9d9d92df36190071fae899a52e4a27e193de41e1
jason3e7 <jason3e7@gmail.com>
2018-02-27 12:40:18 +1300

    69_sqrtx AC bisection

--- 69_sqrtx.c
+++ 69_sqrtx.c
@@ -1,44 +1,40 @@
  /*
   * @file 69_sqrtx.c
   * @author Jason3e7
-  * @algorithm Newton's method
+  * @algorithm bisection
   * @date 201802271052
-  * @note
-  * 
-x^2 - a = 0
-(x, f(x)), Derivative 2x
-
-ex : http://www.matrix67.com/blog/archives/361
-(4, 14), 8
-next x => (y2 - y1) / (x2 - x1) = m
-(14 - 0) / (4 - x) = 8
-4 - x = 14 / 8
-x = 4 - 14 / 8 
-x = 2.25
-
-x-f(x)/(2x), f(x)=x^2-a
-x - (x^2-a)/(2x)
-x - x/2 + a/2x
-(x + a/x)/2 
-
-(4 + 2/4)/2 = 2.25
+  * @note 
+try 2, 3 
+begin == end is different
   */
 
 #include <stdio.h>
 
-int mySqrt(int x) { 
-    long r = x;
-    while (r * r > x)
-        r = (r + x / r) / 2;
-    return r;
-    
+int mySqrt(int x) {
+	if(x == 0 || x == 1) {
+		return x;
+	}
+    int begin = 1, end = x, mid, temp;
+    while (begin <= end) {
+        mid = begin + (end - begin) / 2;
+		temp = x / mid;
+		if(mid == temp) {
+			return mid;
+		}
+		if(mid > temp) {
+			end = mid - 1;
+		} else {
+			begin = mid + 1;
+		}
+	}
+	return end;
 }
 
 int main() {
-	printf("%d\n", mySqrt(0));
-	printf("%d\n", mySqrt(1));
-	printf("%d\n", mySqrt(2));
-	printf("%d\n", mySqrt(10));
+	int i;
+	for(i = 0; i <= 10; i++) {
+		printf("%d = %d\n", i, mySqrt(i));
+	}
 	printf("%d\n", mySqrt(100));
 	printf("%d\n", mySqrt(10000));
 	printf("%d\n", mySqrt(1000000));
