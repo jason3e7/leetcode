8d43f93d1e7f4b39531ec3c702578a8c4dd79642
jason3e7 <jason3e7@gmail.com>
2018-08-29 13:19:35 +0800

    386_lexicographical-numbers fix performance

--- 386_lexicographical-numbers.py
+++ 386_lexicographical-numbers.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+#leetcode 380000
+
 class Solution:
   def lexicalOrder(self, n):
     length = len(str(n))
@@ -9,12 +12,29 @@ class Solution:
       getNum = self.str2int(num[0:nLen])
       if(n >= getNum) :
         output.append(getNum)
+      else :
+        # carry
+        if(nLocate == 0) :
+          break
+        num[nLocate] = '0'
+        nLen -= 1
+        nLocate -= 1 
+        num[nLocate] = chr(ord(num[nLocate]) + 1)
+        # carry
+        for i in [6, 5, 4, 3, 2, 1] :
+          if(ord(num[i]) >= 58) :
+            num[i] = '0'
+            nLen -= 1
+            nLocate -= 1
+            num[i - 1] = chr(ord(num[i - 1]) + 1)
+        continue
       if(nLen < length) :
         nLen += 1
         nLocate += 1
         continue
       if(nLen == length) :
         num[nLocate] = chr(ord(num[nLocate]) + 1)
+        # carry
         for i in [6, 5, 4, 3, 2, 1] :
           if(ord(num[i]) >= 58) :
             num[i] = '0'
@@ -28,3 +48,12 @@ class Solution:
       num *= 10
       num += int(c)
     return num
+
+func = Solution().lexicalOrder
+print(func(1))
+print(func(10))
+print(func(50))
+print(func(200))
+print(func(300))
+print(func(3000))
+print(func(50000))
