1e415070072aed3d27961c5c0733b5aa741dca76
jason3e7 <jason3e7@gmail.com>
2018-11-27 12:41:45 +0800

    914_x-of-a-kind-in-a-deck-of-cards fix structure

--- 914_x-of-a-kind-in-a-deck-of-cards.c
+++ 914_x-of-a-kind-in-a-deck-of-cards.c
@@ -1,22 +1,50 @@
  /*
   * @file 914_x-of-a-kind-in-a-deck-of-cards
   * @author Jason3e7
-  * @algorithm array
+  * @algorithm arrayAndMath
   * @date 201811271144
+  * @note GCD !?
   */
 
 #include <stdio.h>
 #include <stdbool.h>
 
 bool hasGroupsSizeX(int* deck, int deckSize) {
-	int i, j, count[10000] = {0};
+	if(deckSize <= 1) {
+		return false;
+	}
+	if(deckSize == 2) {
+		return (deck[0] == deck[1]);
+	}
+	int i, count[10000] = {0}, point = 0;
 	for(i = 0; i < deckSize; i++) {
 		count[deck[i]]++;
 	}
-	int flag;
-	for(j = 2; j <= deckSize; j++) {
+	int max = 2, min = deckSize;
+	for(i = 0; i < 10000; i++) {
+		if(count[i] == 1) {
+			return false;
+		}
+		if(count[i] != 0) {
+			count[point] = count[i];
+			point++;
+			
+			if(count[i] > max) {
+				max = count[i];
+			}
+			if(count[i] < min) {
+				min = count[i];
+			}
+		}
+	}
+	if(min == max) {
+		return true;
+	}
+
+	int j, flag;
+	for(j = 2; j <= min; j++) {
 		flag = 1;
-		for(i = 0; i < 10000; i++) {
+		for(i = 0; i < point; i++) {
 			if(count[i] % j != 0) {
 				flag = 0;
 				break;
@@ -24,9 +52,9 @@ bool hasGroupsSizeX(int* deck, int deckSize) {
 		}
 		if(flag == 1) {
 			return true;
-		} 
+		}
 	}
-	return false;;
+	return false;
 }
 
 int main() {
@@ -36,5 +64,7 @@ int main() {
 	printf("%d\n", hasGroupsSizeX(num2, 9));
 	int num3[] = {1, 1, 1, 2, 2, 2, 3, 3};
 	printf("%d\n", hasGroupsSizeX(num3, 8));
+	int num4[] = {1, 1, 1, 1, 2, 2, 2, 2, 2, 2};
+	printf("%d\n", hasGroupsSizeX(num4, 10));
 	return 0;
 }
