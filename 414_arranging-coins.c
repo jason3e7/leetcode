e2fb90103505a91081e49664762c1cac0e80eea8
jason3e7 <jason3e7@gmail.com>
2018-12-19 12:16:19 +0800

    414_arranging-coins fix structure

--- 414_arranging-coins.c
+++ 414_arranging-coins.c
@@ -1,27 +1,33 @@
  /*
   * @file 414_arranging-coins
   * @author Jason3e7
-  * @algorithm basic
+  * @algorithm math
   * @date 201812191134
+  * @note
+x^2 + x - 2y = 0
+
+quadratic equation
+
+y = 15 => x = 5, -6
+y = 18 => x = 5.5..., -6.5...
+y = 21 => x = 6, -7
   */
 
 #include <stdio.h>
 
 int arrangeCoins(int n) {
-	int count = 0;
-	while(n > 0) {
-		count++;
+	int count = 1;
+	while(n >= count) {
 		n -= count;
+		count++;
 	}
-	if(n < 0) {
-		count--;
-	}
-	return count;	
+	return count - 1;	
 }
 
 int main() {
 	printf("%d\n", arrangeCoins(5));	
 	printf("%d\n", arrangeCoins(8));	
 	printf("%d\n", arrangeCoins(0));	
+	printf("%d\n", arrangeCoins(1));	
 	return 0;
 }
