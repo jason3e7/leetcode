07a099cfd11d2fa01739226d73fc4b90a9de9a29
jason3e7 <jason3e7@gmail.com>
2018-12-25 10:36:56 +0800

    605_can-place-flowers AC

---
+++ 605_can-place-flowers.c
@@ -0,0 +1,45 @@
+ /*
+  * @file 605_can-place-flowers
+  * @author Jason3e7
+  * @algorithm array
+  * @date 201812251023
+  */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
+	if(n <= 0) {
+		return true;
+	}
+    int i, count = 0;
+	for(i = 0; i < flowerbedSize; i++) {
+		if(flowerbed[i] == 1) {
+			continue;
+		}
+		if(i > 0) {
+			if(flowerbed[i - 1] == 1) {
+				continue;
+			}
+		} 
+		if(i < (flowerbedSize - 1)) {
+			if(flowerbed[i + 1] == 1) {
+				continue;
+			}
+		} 
+		flowerbed[i] = 1;
+		count++;
+		if(count >= n) {
+			return true;
+		}
+	}
+	return false;
+}
+
+int main() {
+	int f1[] = {1, 0, 0, 0, 1};
+	printf("%d\n", canPlaceFlowers(f1, 5, 1));	
+	int f2[] = {1, 0, 1, 0, 1, 0, 1};
+	printf("%d\n", canPlaceFlowers(f2, 7, 0));	
+	return 0;
+}
