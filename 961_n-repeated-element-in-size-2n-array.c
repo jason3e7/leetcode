9508074ddf815c99b9dda83bb5e99b27dcb1a110
jason3e7 <jason3e7@gmail.com>
2018-12-26 11:08:22 +0800

    961_n-repeated-element-in-size-2n-array fix structure

--- 961_n-repeated-element-in-size-2n-array.c
+++ 961_n-repeated-element-in-size-2n-array.c
@@ -1,27 +1,28 @@
  /*
   * @file 961_n-repeated-element-in-size-2n-array
   * @author Jason3e7
-  * @algorithm array, hashMap
+  * @algorithm array, algorithm, hashMap
   * @date 201812261014
+  * @note must xx OR xox appear
+  * but Edge case for 4 elements
   */
 
 #include <stdio.h>
 
 int repeatedNTimes(int* A, int ASize) {
-    int count[10000] = {0};
 	int i, output;
-	for(i = 0; i < ASize; i++) {
-		if(count[A[i]] != 0) {
-			output = A[i];
-			break;	
+	for(i = 2; i < ASize; i++) {
+		if(A[i - 2] == A[i - 1] || A[i - 2] == A[i]) {
+			return A[i - 2];
 		}
-		count[A[i]]++;
 	}
-	return output;
+	return A[3];
 }
 
 int main() {
-	int a[] = {1, 2, 3, 3};
-	printf("%d\n", repeatedNTimes(a, 4));	
+	int a1[] = {1, 2, 3, 3};
+	printf("%d\n", repeatedNTimes(a1, 4));	
+	int a2[] = {1, 2, 4, 3, 3, 3};
+	printf("%d\n", repeatedNTimes(a2, 4));	
 	return 0;
 }
