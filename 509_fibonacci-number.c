d666d1f4b7380ce48b34791911105bc42162a38f
jason3e7 <jason3e7@gmail.com>
2019-01-14 13:28:56 +0800

    509_fibonacci-number fix structure

--- 509_fibonacci-number.c
+++ 509_fibonacci-number.c
@@ -1,20 +1,26 @@
  /*
   * @file 509_fibonacci-number
   * @author Jason3e7
-  * @algorithm math
+  * @algorithm math, DP
   * @date 20190114
   */
 
 #include <stdio.h>
 
+int f[31] = {0};
+
 int fib(int N) {
 	if (N == 0) {
 		return 0;
 	} 
-	if (N == 1) {
+	if (N == 1 || N == 2) {
+		f[N] = 1;
 		return 1;
 	}
-	return fib(N - 1) + fib(N - 2);
+	if(f[N] == 0) {
+		f[N] = fib(N - 1) + fib(N - 2);
+	}
+	return f[N];
 }
 
 int main() {
--- index.md
+++ index.md
@@ -1,2 +1,4 @@
 * \#mtime:20181228, \#rating:80, \#type:c, \#math. [908_smallest-range-i](908_smallest-range-i.c)
   * 很單純的題目, 用到一點點數學.
+* \#mtime:20190114, \#rating:60, \#type:c, \#recursive, \#DP, \#bigNumber. [509_fibonacci-number](509_fibonacci-number.c)
+  * 經典題目, 簡單的可以用遞迴做, 數字很大的時候需要大整數, Dynamic Programming 可以加速.
