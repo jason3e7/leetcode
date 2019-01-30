d9cf9ffcb97495c73f8fa175aeaac63ccf4d4f9d
jason3e7 <jason3e7@gmail.com>
2019-01-30 16:16:56 +0800

    326_power-of-three fix structure

--- 326_power-of-three.c
+++ 326_power-of-three.c
@@ -12,16 +12,11 @@ bool isPowerOfThree(int n) {
 	if(n <= 0) {
 		return false;
 	}
-	while(n >= 3) {
-		if(n % 3 != 0) {
-			return false;
-		}
-		n /= 3;
+	long temp = 1;
+	while(n > temp) {
+		temp *= 3;
 	}
-	if(n == 1) {
-		return true;
-	}
-	return false;
+	return n == temp;
 }
 
 int main() {
